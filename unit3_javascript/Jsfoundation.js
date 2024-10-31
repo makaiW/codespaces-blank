48d151d6ed6dd1d644653024b8f3664262a083a1
makaiW <144815658+makaiW@users.noreply.github.com>
2024-10-31 17:41:23 +0000

    learned bout javascriple

---
+++ unit3_javascript/Jsfoundation.js
@@ -0,0 +1,35 @@
+// Data type = basic building blocks of javaScript 
+// programming language \
+
+
+// string-are pieces of data bound together by quotation 
+"this is a sentence wrapped in quotes-this is a string"
+"123231- this is a number but when in quotes it is a string "
+
+//integer = numbers- doesnt matter if its whole number
+// our a decimal
+88678
+987
+1235.0897
+0
+//boolean = true or false
+true,1
+false,0
+
+// variables = are containers for data 
+// when writing a variable we break it down into 3 steps
+//1. declare the type of variables we're using
+//2.we need to give variables names
+//3.we need a equal sign
+//4.we need to give its a value/ data
+var a ='//universaal container- cant go wrong using this'
+let b  ='// temporary variables- data that will change'
+const c = '// constant variables- data that will not change'
+
+let weather_by_city = 'philly'
+let weather_by_current_temp = 80
+let weather_by_day = 'thursday'
+const logo= '.weatherchannellogo.jpg'
+const saved_currentCity = 'philly'
+var w10day = true
+print('sending message to terminal.')
