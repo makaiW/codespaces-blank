90142ec350b5c1adc005e6a4c11bfb9a69febc0d
makaiW <144815658+makaiW@users.noreply.github.com>
2024-11-07 18:41:04 +0000

    completed js quiz

---
+++ jsPopQuiz.js
@@ -0,0 +1,61 @@
+// RULES
+// You are ONLY permitted to use W3 schools. 
+// You may NOT use any other external websites.
+// No music is permitted during the quiz. 
+// All phones must be put away- If a phone is visible, you will recieve a verbal warning.
+// A second warning will result in an automatic failure. 
+// Once you've completed your quiz, you may use the computer at your leisure- PLEASE DO NOT
+// disrupt/ distract your classmates who are still taking the quiz. 
+ 
+// 1. How do you declare a variable in JavaScript? Please write your answer 
+// using JavaScript code. 
+
+'you can the declare a variable by its funcations'
+'string can store text such as "Hello"'
+'int - stores integers whole numbers such as 123'
+'float - stores floating point numbers, with decimals just as 19.99'
+'char - stores single characters, such as 'a'  or 'B' '
+'boolean - stores values with two states true or false'
+
+// 2. How would you declare a variable where the data WILL NOT change in JavaScript?
+// Please write your answer using JavaScript code. 
+Boolean(25>10)
+// 3. Identify the following JavaScript operators and describe when you would use it?
+// Please write your answers using the string data type. 
+   let y = 5;
+   let x = y + 2;
+   const x + y = 
+//  
+// 4. Describe a scenario where you would use a comparison operator,
+// and then provide a code example based on the scenerio you provided.
+
+//I went to the store with 20 dollors and Ispent 16.25 now how much do I have left over
+var x = 20;
+x -= 16.25;
+
+// 5. Describe a scenario where you would use a logical operator,
+// and then provide a code example based on the scenerio you provided. 
+
+let x = 6;
+let y = 3;
+
+(x < 10 && y > 1) + "<br>" + 
+(x < 10 && y < 1);
+
+
+// 6. Create three variables where when combined will result in the value of 36. 
+// Please use the console.log () to out put your result. 
+
+var a =""
+var b = ""
+var c = ""
+console.log(a+b+c)
+// 7. Fill in the variable that will output the appropriate result. 
+//var x ='Please replace this text with your answer.'
+//var y = x *2
+
+// Your console log should output the value 28.
+console.log(y)
+// 8. What does it "concatenation" mean? Please write your response as a string
+// and also provide a code example of two variables being concatenated and output the result
+// using the console.log() function. 
---
+++ unit3_javascript/asyncActivity_Nov1.js
@@ -0,0 +1,36 @@
+
+//1. Describe the difference between var, let, and const. Your answer can be
+//The difference between var, let is a data that can change
+//while const has data that wont change
+
+
+
+
+//2. Use what you've learned about data types and variables and create three 
+//variables that when multiplied together will result in the value 4000. 
+//(2 x 2 x 1000 = 4000)
+//keywords
+//terminal
+//console.log()funcation
+//variable 
+//w3schools
+//data type
+
+
+var a=2
+var b=2
+var c= 1000
+console.log(a*b*c)
+
+
+//Research the console.log() function on w3schools to output your result in the terminal. 
+//3. Describe a scenario where you would use the let variable and when you 
+//would use a const variable in a computer program. Your answer can be
+//ormatted as a comment or a string. 
+//A scenario where the let variable would be used is the date on your device. A scenario where the const variable would be used is the calculator apps format.'
+
+
+//4. If there were three things you that feel could improve the class, what would those things be and why?
+//Your answer can be formatted as a comment or a string. 
+//there isnt anything i would change bout this class
+//i dont think there is anything you can change bout this class
