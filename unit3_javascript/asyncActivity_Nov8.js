dfbd1ce1c953323dce8a92d25d45a668fd0d5ce3
makaiW <144815658+makaiW@users.noreply.github.com>
2024-11-08 18:23:48 +0000

    completed the async assigment

---
+++ unit3_javascript/asyncActivity_Nov8.js
@@ -0,0 +1,21 @@
+
+let a = '7';
+let b = '100';
+console.log(a+b)
+
+
+
+//@@ -40,7 +44,9 @@ to get the requested result.
+- //which logical operator would you use to reflect that the result between the code expressions are **true**. use the print function to output the result in the termial, then explain why you would use that particular logical operator using a string.
+
+
+z= y*2
+
+(x > 1 && z==4 )
+//@@ -49,7 +55,9 @@ x > 1 _____ z==4
+ //which logical operator would you use to reflect that the result between the code expressions below are **false**. Use the console.log() function to output the result in the termial, then explain why you would use that particular logical operator using a string.
+
+var x= 12
+var y= 2
+var z= 12/4
+(x <= 12 || z < 10)
