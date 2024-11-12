002bf3b98f579a21fed0028ecc05371af024ed26
makaiW <144815658+makaiW@users.noreply.github.com>
2024-11-12 18:40:25 +0000

    reuploading assigment becasue I did the wrong class assigment

--- unit3_javascript/asyncActivity_Nov8.js
+++ unit3_javascript/asyncActivity_Nov8.js
@@ -1,21 +1,50 @@
 
-let a = '7';
-let b = '100';
+//What does it "string concatenation" mean? Please write your response as a string and also provide a code example of two variables being concatenated and output the result in your terminal.
+
+'string concatenation is when you combine two or more stringd with + (addition) operator'
+
+
+
+//`Please list and describe three rules for creating variable names. Please provide your answer as a string.
+'1. variables names cannot start with a number'
+'2.variable names cannot have any white space'
+'3.variable names cannot be reserved words/keywords'
+
+
+
+//Define each of the following variable name formats then provide a code example of how you would write them.
+
+//Camel case - every word after the first word is capitalized
+ var username=""
+ var timeRemininginQre=""
+//Snake case - every word is seperated by an underscore
+var User_name=""
+var time_remining_in_qrtr=""
+
+// Pascal case
+var user_name=''
+var time_remining_in_qrtr=''
+
+
+
+let a = '350';
+let b = a*2
 console.log(a+b)
 
 
 
 //@@ -40,7 +44,9 @@ to get the requested result.
 - //which logical operator would you use to reflect that the result between the code expressions are **true**. use the print function to output the result in the termial, then explain why you would use that particular logical operator using a string.
+x=10
 
-
+y=2
 z= y*2
 
-(x > 1 && z==4 )
+console.log(x > 1 && z==4 )
 //@@ -49,7 +55,9 @@ x > 1 _____ z==4
  //which logical operator would you use to reflect that the result between the code expressions below are **false**. Use the console.log() function to output the result in the termial, then explain why you would use that particular logical operator using a string.
 
 var x= 12
 var y= 2
-var z= 12/4
-(x <= 12 || z < 10)
+var z= 12/y
+console.log(x <= 12 || z < 10)
