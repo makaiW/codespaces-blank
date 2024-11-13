647d95447516b22cc9114fca13e8ad1d9d45cc10
makaiW <144815658+makaiW@users.noreply.github.com>
2024-11-13 18:42:23 +0000

    Review javascript operator

---
+++ unit3_javascript/operatorReview.js
@@ -0,0 +1,49 @@
+//Artithmrthic operator - process that involve math.
+let musicSubscription_1 = 17.00
+let bankAccount = 230.00
+ function  withdrawMusicSub()
+// if money exists-withdraw money for sub 
+console.log(bankAccount - musicScriptionPlan_1);
+//if no money - pause sub 
+
+//Assigment operators - applying a value to variable
+//MAIN TAKEAWAY : the single equal symbol/sign is used for assignment
+
+var jeans = 200.00
+jeans *=.25
+var discount = (jeans *=25);
+
+
+
+
+
+
+var total = (jeans = discount);
+console.log(total);
+
+
+var jeans = 200.00
+
+var discount = .45
+var caLculatedDiscount = jeans * discount
+var total = jeans - calculatedDiscount
+console.log(total)
+
+// comparison Operation - analying values to one another 
+// the equal symbol has special propertis in js.\
+// single equal (=): only for assignment
+var number = 4
+// double equal (==): same as or equal to
+var a = 10
+var b = 12
+// console.log(a==b)
+var userPassword = '1234'
+var dbPassword = '12323'
+console.log(userPassword== dbPassword)
+
+
+// triple equal (===): equal value and equal type 
+var depositAmount = '400.00'
+var savingAccount = 400.00
+console.log( depositamount === savingaAccount)
+// exclainmation equal (!=): not equal 
