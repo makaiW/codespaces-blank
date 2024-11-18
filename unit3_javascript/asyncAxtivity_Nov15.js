53e966f7d6b5d469a72db6f2e652b858f77337eb
makaiW <144815658+makaiW@users.noreply.github.com>
2024-11-18 17:42:13 +0000

    completed the async assigment and fixed the answers

--- unit3_javascript/asyncAxtivity_Nov15.js
+++ unit3_javascript/asyncAxtivity_Nov15.js
@@ -3,17 +3,27 @@
     //Which operator would be used here? Please provide a code example of how you would write this and output the result using the print() function for python or the console.log() function for javascript? 
     
 
-    //you can use a comparison Operatior becasue you can analying values to one another  
- 'A way you can use this is like  '
-  var userPassword = '1234'
- var dbPassword = '12323'
- console.log(userPassword== dbPassword)
+   
+ //you can use Logical operator will need to return true 
+ var name= ''
+ var password=''
 
+ var secutitySystemName='makai' 
+ var securitySystemPassword='123'
+ console.log9(secutitySystemName+securitySystemPassword)
+2.//A hospital needs to keep track of medical equipment. they are getting a shipment of new ECG machines and Oxygen tanks and need to verify. If they have more than whats needed in their office, they need to send the overflow of equipment back to the manufacturer, but if they have less, the need to request more. In this scenario, they are supposed to have 10 ECG machines, but only recieved 4, and for the oxygen tanks they are supposed to have 6 in stock, but recieved 9. Which operator would be used here? Please provide a code example of how you would write this and output the result using the print() function for python or the console.log() function for javascript?
+   
+"comparison operator"
 
+3. //A company is developing a messaging app that allows people to send text message for free. They need to allow users to capture the user data and then send it to someone else. Which operator would be used here? Please provide a code example of how you would write this and output the result using the print() function for python or the console.log() function for javascript?
+    msgOrigin='hello'
+msgReciever = msgOrigin
+console.log(msgReciever)
 
-
-    // for student's learning JavaScript
+// for student's learning JavaScript
     //# for student's learning JavaScript
     // 4. Identify and explain what each of these symbols are
-    - = //The = sign is assign value to a varable 
-    - && //The && operator returns true if both expressions are true, otherwise it returns false.
+    - = 'The = sign is assign value to a varable'
+    - &&  'logical operator'
+   - || 'logical'
+   -  == 'comparsion operator '
