0ec4e4a8cc017cfc9e2f931752500bd6f2754e31
makaiW <144815658+makaiW@users.noreply.github.com>
2024-12-11 18:32:01 +0000

    programming my grade function

---
+++ unit3_javascript/jsConditionerReview.js
@@ -0,0 +1,31 @@
+// a funcation is a set of insteration 
+
+
+
+
+//function timeOfday(time) {
+   // if (time == "am") {
+       // console.log("it is the morning");
+
+   // } else if (time == "pm")
+        //console.log("it is the evening");
+   // else {
+    //    console.log("sorry i cant help you ")
+   // }
+//}
+//timeOfday();
+
+
+function Grades(grade) {
+    if (grade == "100-90") {
+        console.log("this grade is a A");
+    } else if (grade == "89-80")
+        console.log("This grade is a B");
+    else if (grade == "79-70") {
+        console.log("This grade is a C");
+    } else if (grade == "69-60")
+        console.log("This grade is a D");
+  else if (grade == "59-0")
+    console.log("This grade is a F")
+}
+ Grades(98);
