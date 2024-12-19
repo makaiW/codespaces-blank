08e0531a64c9f4520ec4df046cdd11fc85b92560
makaiW <144815658+makaiW@users.noreply.github.com>
2024-12-19 18:36:12 +0000

    completed extra credit assignment

---
+++ unit3_javascript/ holidayExtraCredit.js
@@ -0,0 +1,20 @@
+function Calender(ImportantsDates) {
+    if (Calender == "December 20") {
+ console.log("last day of school");
+    } else if (Calender == "December 21th")
+        console.log("Im going to chill");
+    else if (Calender == "December 24th") {
+        console.log("Going to moms houes for christmas eve");
+    } else if (Calender == "December 25th")
+        console.log("Going to moms house to open gifts");
+} else if (Calender == "December 28th")
+        console.log("Have a eye doctors appoinment at 10:30");
+   else if (Calender == "june 1th")
+    console.log("New years day");
+
+  else if   (Calender == "June 7th")
+    console.log("Return back to school");
+
+
+
+ImportantsDates();
