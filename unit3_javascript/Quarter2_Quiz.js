d5a7cafcc2d7460a42723af4d9bf53c8cc5106d9
makaiW <144815658+makaiW@users.noreply.github.com>
2024-12-20 18:41:53 +0000

    submiiting Q2 quiz

---
+++ unit3_javascript/Quarter2_Quiz.js
@@ -0,0 +1,31 @@
+"1. Is a set of instructions that perform a specific task or a job"
+
+
+ "2. Function parameters are the names listed in the function definition and a function argument is a real value that pass to a function"
+
+ "3.IF' and 'ELSE' keywords. they allow us to  make decesion in our funcation based on the data we recieve "
+ "4. integer is numbers doesnt matter if its whole number even can be  a decimal"
+ "5. a boolen is for a true or false statements "
+"6. a logical opeatar would work best for a age verification funcation"
+"7.A comparison Operation can work for a office security function"
+"8."
+"9."
+function federalIncome(taxes ){
+    if (taxes === "0.00 and  12000.00"){
+        console.log("they will be taxed 10%, plus an additional 3% for the state.")
+             var federalIncome = taxed *0.10;
+             console.log(federalIncomet);
+             var total = taxed - federalIncome;
+             console.log(total);
+    
+    } else if(taxes == "12001.00 andf 50000.00 ")
+    console.log("they will be taxed 12%, plus an additional 3% for the state.");
+     var  federalIncome = taxed *0.12;
+             console.log(federalIncome);
+             var total = taxed - federalIncome;
+             console.log(total);
+    
+    }    else if(federalIncome === "50001.00 and 103000.00")
+        console.log("they will be taxed 22%, plus and additional 3% for the state.");
+    else if 
+        
