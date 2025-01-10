a48733d110aac32977775386660b87af80061032
makaiW <144815658+makaiW@users.noreply.github.com>
2025-01-10 18:39:57 +0000

    submitting assigment

---
+++ unit 4/asynceActivityJan10.js
@@ -0,0 +1,28 @@
+
+
+
+//1
+var groceyStore = ['apple','frozen pizza','','ice','frozen waffle','frozen broccii','frozen mango','orange','banana','pineapple,','peach']
+    var index1 =["apple"]
+    var index2 = ["frozen pizza"]
+    var index3 = ["ice"]
+    var index4 = ["frozen waffle"]
+    var index5 = ["frozen brocci"]
+    var index6 = ["frozen mango"]
+    var index7 = ["orange"]
+    var index8 = ["banana"]
+    var index9 = ["pineapple"]
+    var index10 = ["peach"]
+
+    console.log(groceyStore);
+
+//2
+    numberList = [1,30,39,50,293,100]
+    var index1 =["1"]
+    var index2 = ["30"]
+    var index3 = ["39"]
+    var index4 = ["50"]
+    var index5 = ["100"]
+    var index6 = ["293"]
+
+    console.log(numberList)
