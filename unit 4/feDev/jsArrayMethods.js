3866492f58d8a60c72ee24157a1d5614c2b6aba6
makaiW <144815658+makaiW@users.noreply.github.com>
2025-01-16 18:40:38 +0000

    completed work

---
+++ unit 4/feDev/jsArrayMethods.js
@@ -0,0 +1,40 @@
+//js array are called arrays
+// A JS array is collection date type for storing different types of data 
+// //they are more flexible compared to other programming
+//language list data type
+// ex
+
+classes= ["math",
+    "reading",
+    "gym",
+    10,
+    10,23
+    true,
+    "reading"
+
+];
+var bookbag = ['pencils','pen','notebooks','fiction book']
+bookBag.push('sketch book')
+console.log(bookBag)
+
+// Methods = A special type of funtion that works
+// on lists and objects.
+
+// push() method - a special function that allow us to 
+// add an item to a list. the new piece of data will always
+// go to the back / end of the list.
+
+
+// LIFO (Last In, First Out)
+
+
+// pop() method - a special function that allows us to 
+// remove an item from a list. The most recent item, usually
+// at the end of the list, will be removed.
+// the pop method DOES NOT need an argument - it will always
+// remove the last item.
+bookBag.pop()
+console.log(bookbag)
+
+//lenth funcation- a special funcation that counts the number of item inside the list
+
--- unit 4/feDev/logic.js
+++ unit 4/feDev/logic.js
@@ -1,6 +1,6 @@
 console.log("js connected")
 
 var msg = "welcome to front end development"
-   
+
 document.getElementById("demo").innerHTML = "welcome to front end development";
 
