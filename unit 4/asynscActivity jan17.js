0481f6f6b7916469a2bac3fd2f742e40b954e4bc
makaiW <144815658+makaiW@users.noreply.github.com>
2025-01-17 18:39:00 +0000

    completed asnyc work

---
+++ unit 4/asynscActivity jan17.js
@@ -0,0 +1,24 @@
+//1.
+
+
+
+
+
+
+
+//2.
+var campingSupplies = ['tent','sleeping bag','flash light','camping knife']
+campingSupplies.push('extra clothes')
+console.log(campingSupplies)
+
+
+//3
+
+
+
+
+
+//4.
+var campingSupplies = ['tent','sleeping bag','flash light','camping knife']
+campingSupplies.pop('sleeping bags')
+console.log(campingSupplies)
